80f86a8363980ca19d829e29bf02cacb26c9dcfa
wt <14746915@qq.com>
2022-11-04 19:18:36 +0800

    feat: init

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ color: 'green', textAlign: 'center', fontSize: 32 }}> wql_cli，一个react模板脚手架</div >
+  );
+}
+
+export default App;
